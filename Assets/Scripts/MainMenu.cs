0f6fde7542c4f5516310243f63e66ffe28e8d21c
najongjine <54340913+najongjine@users.noreply.github.com>
2022-02-11 09:10:57 +0900

    updated energy recharge system

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,16 +4,17 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text highScoreText;
     [SerializeField] private TMP_Text energyText;
-    [SerializeField] private int maxEnergy;
-    [SerializeField] private int energyRechargeDuration;
-
+    [SerializeField] private Button playButton;
     [SerializeField] private AndroidNotificationHandler androidNotificationHandler;
     [SerializeField] private IOSNotificationHandler iosNotificationHandler;
+    [SerializeField] private int maxEnergy;
+    [SerializeField] private int energyRechargeDuration;
 
     private int energy;
 
@@ -22,15 +23,26 @@ public class MainMenu : MonoBehaviour
 
     private void Start()
     {
+        OnApplicationFocus(true);
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        if (!hasFocus) { return; }
+        CancelInvoke();
+
         int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
+
         highScoreText.text = $"High Score: {highScore}";
 
-        energy=PlayerPrefs.GetInt(EnergyKey,maxEnergy);
+        energy = PlayerPrefs.GetInt(EnergyKey, maxEnergy);
 
-        if (energy < 1)
+        if (energy == 0)
         {
-            string energyReadyString=PlayerPrefs.GetString(EnergyReadyKey,string.Empty);
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);
+
             if (energyReadyString == string.Empty) { return; }
+
             DateTime energyReady = DateTime.Parse(energyReadyString);
 
             if (DateTime.Now > energyReady)
@@ -38,9 +50,24 @@ public class MainMenu : MonoBehaviour
                 energy = maxEnergy;
                 PlayerPrefs.SetInt(EnergyKey, energy);
             }
+            else
+            {
+                playButton.interactable = false;
+                Invoke(nameof(EnergyRecharged), (energyReady - DateTime.Now).Seconds);
+            }
         }
+
         energyText.text = $"Play ({energy})";
     }
+
+    private void EnergyRecharged()
+    {
+        playButton.interactable = true;
+        energy = maxEnergy;
+        PlayerPrefs.SetInt(EnergyKey, energy);
+        energyText.text = $"Play ({energy})";
+    }
+
     public void Play()
     {
         if (energy < 1) { return; }
@@ -53,14 +80,11 @@ public class MainMenu : MonoBehaviour
         {
             DateTime energyReady = DateTime.Now.AddMinutes(energyRechargeDuration);
             PlayerPrefs.SetString(EnergyReadyKey, energyReady.ToString());
-
 #if UNITY_ANDROID
             androidNotificationHandler.ScheduleNotification(energyReady);
 #elif UNITY_IOS
             iosNotificationHandler.ScheduleNotification(energyRechargeDuration);
 #endif
-
-
         }
 
         SceneManager.LoadScene(1);
